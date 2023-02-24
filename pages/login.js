8dd65ad9b1b894fd08dab72b7989df9b845de490
kiyo1115 <lewisfeet@gmail.com>
2023-02-24 14:39:51 +0900

    4th comment

---
+++ pages/login.js
@@ -0,0 +1,90 @@
+import { useContext, useState } from "react";
+import AppContext from "../context/AppContext";
+import { login } from "../lib/auth";
+import Link from "next/link";
+
+const Login = () => {
+  // const appContext = useContext(AppContext);
+  // const [data, setData] = useState({ identifier: "", password: "" });
+  // const handleLogin = () => {
+  //   login(data.identifier, data.password).then((res) => {
+  //     appContext.setUser( res.data.user );
+  //   }).catch((err) => console.log(err));
+  // };
+  // const handleChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value });
+  // };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <div className="paper">
+            <div className="header">
+              <h2>ログイン</h2>
+            </div>
+          </div>
+          <section className="wrapper">
+            <form>
+              <fieldset>
+                <div>
+                  <label>メールアドレス：</label>
+                  <input
+                    type="email"
+                    name="identifier"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label>パスワード：</label>
+                  <input
+                    type="password"
+                    name="password"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  />
+                </div>
+                <span>
+                  <Link href="/">
+                    <small>パスワードをお忘れですか？</small>
+                  </Link>
+                </span>
+                <button
+                  style={{ float: "right", width: 120 }}
+                  color="primary"
+                  onClick={() => {
+                    handleLogin();
+                  }}
+                >
+                  ログイン
+                </button>
+              </fieldset>
+            </form>
+          </section>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .paper {
+            text-align: center;
+            margin-top: 50px;
+          }
+          .header {
+            width: 100%;
+            margin-bottom: 30px;
+          }
+          .wrapper {
+            padding: 10px 30px 20px 30px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,101 @@
+import { useContext, useState } from "react";
+import AppContext from "../context/AppContext";
+import { registerUser } from "../lib/auth";
+
+
+const register = () => {
+  // const appContext = useContext(AppContext);
+  // const [data, setData] = useState({ username: "", email: "", password: "" });
+
+  // const handleRegister = () => {
+  //   registerUser(data.username, data.email, data.password)
+  //     .then(() => {
+  //       //auth.jsでもthenで処理しているため、こちらでも成功or失敗の記述をする
+  //       appContext.setUser(res.data.user); //_app.jsのステートのデータを参照
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+
+  return (
+    <div>
+      <div>
+        <div>
+          <div className="paper">
+            <div className="header">
+              <h2>ユーザー登録</h2>
+            </div>
+          </div>
+          <section className="wrapper">
+            <from>
+              <fieldset>
+                <div>
+                  <label>ユーザー名：</label>
+                  <input
+                    type="text"
+                    name="username"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => {
+                      setData({ ...data, username: e.target.value });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label>メールアドレス：</label>
+                  <input
+                    type="email"
+                    name="email"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => {
+                      setData({ ...data, email: e.target.value });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label>パスワード：</label>
+                  <input
+                    type="password"
+                    name="password"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => {
+                      setData({ ...data, password: e.target.value });
+                    }}
+                  />
+                </div>
+                <span>
+
+                </span>
+                <button
+                  style={{ float: "right", width: 120 }}
+                  color="primary"
+                  onClick={() => {
+                    handleRegister();
+                  }}
+                >
+                  登録
+                </button>
+              </fieldset>
+            </from>
+          </section>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .paper {
+            text-align: center;
+            margin-top: 50px;
+          }
+          .header {
+            width: 100%;
+            margin-bottom: 30px;
+          }
+          .wrapper {
+            padding: 10px 30px 20px 30px;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default register;
