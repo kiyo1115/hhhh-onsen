313c61f1668a8c2e57341542ec52dfef83c1bc92
kiyo1115 <lewisfeet@gmail.com>
2023-02-28 14:24:58 +0900

    17th

--- apiCalls.js
+++ apiCalls.js
@@ -7,7 +7,7 @@ export const loginCall = async (user, dispatch) => {
     
     //データベースと通信できたら第一引数のaxios経由でURL/loginを開き
     //第二引数（user）にはバックエンドの(req.body)に代入され、{emailとpassword}が入る
-    const res = await axios.post("http://localhost:5000/auth/login", user);
+    const res = await axios.post("http://localhost:5000/api/auth/login", user);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     //axiosを使用した場合、ログインしたらdataオブジェクトに格納されるため
     // res.dataを更新する
